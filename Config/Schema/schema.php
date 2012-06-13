18075b0f6ef61e09a3fd7e33aea757707b21ca71
Codemis <johnathan@still-water.com>
2012-06-13 14:29:41 -0700

    Removed sql_dump

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -13,6 +13,7 @@ class ApiVideoTranslatorSchema extends CakeSchema {
 		'token' => array('type' => 'string', 'null' => false, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'expires_at' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
 		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
 	);
