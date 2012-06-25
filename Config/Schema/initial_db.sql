25d2037bb7514b2c0d5a3f6e30bd969942b5b5f3
Codemis <johnathan@missionaldigerati.com>
2012-06-25 15:35:56 -0700

    Fixed a bug in the dump file.

--- Config/Schema/initial_db.sql
+++ Config/Schema/initial_db.sql
@@ -1,42 +1,85 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.1.44)
+# Database: cake_api_video_translator
+# Generation Time: 2012-06-25 22:31:34 +0000
+# ************************************************************
 
 
-DROP TABLE IF EXISTS `cake_api_video_translator`.`clips`;
-DROP TABLE IF EXISTS `cake_api_video_translator`.`master_recordings`;
-DROP TABLE IF EXISTS `cake_api_video_translator`.`translation_requests`;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
-CREATE TABLE `cake_api_video_translator`.`clips` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`translation_request_id` int(11) NOT NULL,
-	`audio_file_location` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`video_file_location` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`completed_file_location` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT 'PENDING' NOT NULL,
-	`created` datetime DEFAULT NULL,
-	`modified` datetime DEFAULT NULL,
-	`completed` datetime DEFAULT NULL,	PRIMARY KEY  (`id`)) 	DEFAULT CHARSET=latin1,
-	COLLATE=latin1_swedish_ci,
-	ENGINE=MyISAM;
+# Dump of table clips
+# ------------------------------------------------------------
 
-CREATE TABLE `cake_api_video_translator`.`master_recordings` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`translation_request_id` int(11) NOT NULL,
-	`title` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`language` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`final_filename` text CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
-	`status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT 'PENDING',
-	`modified` datetime DEFAULT NULL,
-	`created` datetime DEFAULT NULL,
-	`completed` datetime DEFAULT NULL,	PRIMARY KEY  (`id`)) 	DEFAULT CHARSET=latin1,
-	COLLATE=latin1_swedish_ci,
-	ENGINE=MyISAM;
+DROP TABLE IF EXISTS `clips`;
 
-CREATE TABLE `cake_api_video_translator`.`translation_requests` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`token` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-	`created` datetime DEFAULT NULL,
-	`modified` datetime DEFAULT NULL,
-	`expires_at` datetime DEFAULT NULL,	PRIMARY KEY  (`id`)) 	DEFAULT CHARSET=latin1,
-	COLLATE=latin1_swedish_ci,
-	ENGINE=MyISAM;
+CREATE TABLE `clips` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `translation_request_id` int(11) NOT NULL,
+  `audio_file_location` text,
+  `video_file_location` text,
+  `completed_file_location` text,
+  `status` varchar(255) NOT NULL DEFAULT 'PENDING',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `completed` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+
+
+# Dump of table master_recordings
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `master_recordings`;
+
+CREATE TABLE `master_recordings` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `translation_request_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `final_filename` text,
+  `status` varchar(255) DEFAULT 'PENDING',
+  `modified` datetime DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `completed` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table translation_requests
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `translation_requests`;
+
+CREATE TABLE `translation_requests` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `expires_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
